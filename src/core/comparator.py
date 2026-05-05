51a7abdbac3c8d240b4ef2f5165a7782ac13f272
Fajar Oktavian Ramadhan <161268383+fajarr1110@users.noreply.github.com>
2026-05-05 18:31:43 +0700

    Delete src directory

--- src/core/comparator.py
+++
@@ -1,2 +0,0 @@
-def compare(hash1, hash2):
-    return hash1 == hash2
--- src/core/hashing.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-def md5_hash(text):
-    return hashlib.md5(text.encode()).hexdigest()
-
-def sha256_hash(text):
-    return hashlib.sha256(text.encode()).hexdigest()
