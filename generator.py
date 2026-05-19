9aab32dfa95ea0561d1826eec48557a6e2131a1d
Fajar Oktavian Ramadhan <fajaroktavian110@gmail.com>
2026-05-19 22:06:07 +0700

    implement CUDA candidate generator

--- generator.py
+++ generator.py
@@ -1,9 +1,9 @@
 import numpy as np
 from numba import cuda
 
-# kamu bisa ubah charset sesuai kebutuhan (proposal: huruf kecil + angka)
 CHARSET = "abcdefghijklmnopqrstuvwxyz"
 CHARSET_SIZE = len(CHARSET)
+
 
 @cuda.jit
 def generate_candidates(indices, length, output):
